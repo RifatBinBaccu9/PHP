e94a22726ec00ed99166c6e72323964178e8c63e
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2024-10-17 11:50:41 +0600

    FD

---
+++ Conditionals(if,else,switch).php
@@ -0,0 +1,28 @@
+<?php
+
+$x=6;
+$y=9;
+
+// if else section
+// if($x > $y){
+//     echo 'x is larges valu';
+// }else{
+//     echo 'y is larges valu';
+// }
+
+// switch section
+ switch($x){
+    case 9:
+     echo "it's x valu";
+     break;
+
+     case 7:
+        echo "it's x valu";
+        break;
+    
+    default:
+        echo "it's x valu";
+        break;
+ }
+
+?>
